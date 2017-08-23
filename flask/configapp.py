c8891cf20a772233aec1f190a979bfa63ee16506
hclent <hclent1@gmail.com>
2017-08-23 11:05:39 -0700

    Commented out celery.
    Will add Celery stuff later, but for now I'm commenting this out.

--- flask/configapp.py
+++ flask/configapp.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from celery import Celery
 import logging
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy import exc, event, select
 
 app = Flask(__name__, static_url_path='/hclent/Webdev-for-bioNLP-lit-tool/flask/static')
 app.config.from_pyfile('/home/hclent/repos/Webdev-for-bioNLP-lit-tool/configscke.cfg', silent=False) #pass abs path
+
+# TODO: make sure celery is configured correctly
+# celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+# celery.conf.update(app.config)
 
 logging.basicConfig(filename='.app.log',level=logging.DEBUG)
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
@@ -73,3 +78,30 @@ def ping_connection(connection, branch):
 		# restore "close with result"
 		connection.should_close_with_result = save_should_close_with_result
 
+
+#TODO: Celery tasks for asynch jobs, modal message updates, & sending e-mails
+# @celery.task
+# def greeting():
+# 	print("hello i have no idea what i'm doing")
+#
+# task = greeting.apply_async(countdown=10)
+
+# @celery.task(bind=True)
+# def long_task(self):
+# 	"""Background task that runs a long function with progress reports."""
+# 	verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
+# 	adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
+# 	noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
+# 	message = ''
+# 	total = random.randint(10, 50)
+# 	for i in range(total):
+# 		if not message or random.random() < 0.25:
+# 			message = '{0} {1} {2}...'.format(random.choice(verb),
+# 											  random.choice(adjective),
+# 											  random.choice(noun))
+# 		self.update_state(state='PROGRESS',
+# 						  meta={'current': i, 'total': total,
+# 								'status': message})
+# 		time.sleep(1)
+# 	return {'current': 100, 'total': 100, 'status': 'Task completed!',
+# 			'result': 42}
